01a26e7491afde53855f1c129f0902ec17993bc4
Arund793 <genesis793@naver.com>
2025-04-03 16:49:04 +0900

    0403(2)

--- src/App.js
+++ src/App.js
@@ -1,52 +1,119 @@
 import { useState } from 'react';
-import MyB from './MyButton';
-import { Button1, Button3 } from './ButtonLib';
-import AboutPage from './AboutPage';
-import Profile from './Profile';
-import ShoppingList from './ShoppingList';
-import CountState from './CountState';
-import CountState2 from './CountState2'; // 👈 추가! CountState2 임포트
 
-import './App.css';
+function Square({ value, onSquareClick }) {
+    return (
+        <button className="square" onClick={onSquareClick}>
+            {value}
+        </button>
+    );
+}
 
-export default function App() {
-  const [count, setCount] = useState(0);
+function Board({ xIsNext, squares, onPlay }) {
+    function handleClick(i) {
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
+        const nextSquares = squares.slice();
+        if (xIsNext) {
+            nextSquares[i] = 'X';
+        } else {
+            nextSquares[i] = 'O';
+        }
+        onPlay(nextSquares);
+    }
 
-  function handleClick() {
-    setCount(count + 1);
-  }
+    const winner = calculateWinner(squares);
+    let status;
+    if (winner) {
+        status = 'Winner: ' + winner;
+    } else {
+        status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+    }
 
-  return (
-      <div className="wrapper">
-        <h1>Welcome to my app</h1>
-        <div>
-          <p>default export example</p>
-          <MyB />
+    return (
+        <>
+            <div className="status">{status}</div>
+            <div className="board-row">
+                <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+                <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+                <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+            </div>
+            <div className="board-row">
+                <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+                <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+                <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+            </div>
+            <div className="board-row">
+                <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+                <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+                <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+            </div>
+        </>
+    );
+}
 
-          <p>named export example</p>
-          <Button1 /> &nbsp;
-          <Button3 />
+export default function Game() {
+    const [history, setHistory] = useState(() => [Array(9).fill(null)]);
+    const [currentMove, setCurrentMove] = useState(0);
+    const xIsNext = currentMove % 2 === 0;
+    const currentSquares = history[currentMove];
 
-          <p>wrapping example:</p>
-          <AboutPage />
-          <Profile />
+    function handlePlay(nextSquares) {
+        const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+        setHistory(nextHistory);
+        setCurrentMove(nextHistory.length - 1);
+    }
 
-          <p>Rendering lists</p>
-          <ShoppingList />
+    function jumpTo(nextMove) {
+        if (currentMove === nextMove) {
+            return;
+        }
+
+        setCurrentMove(nextMove);
+    }
+
+    const moves = history.map((squares, move) => {
+        let description;
+        if (move > 0) {
+            description = 'Go to move #' + move;
+        } else {
+            description = 'Go to game start';
+        }
+        return (
+            <li key={move}>
+                <button onClick={() => jumpTo(move)}>{description}</button>
+            </li>
+        );
+    });
+
+    return (
+        <div className="game">
+            <div className="game-board">
+                <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+            </div>
+            <div className="game-info">
+                <ol>{moves}</ol>
+            </div>
         </div>
+    );
+}
 
-        <div>
-          <p>Updating the screen</p>
-          <CountState />
-          <CountState />
-          <CountState />
-        </div>
-
-        <div>
-          <p>Sharing data between components</p>
-          <CountState2 count={count} onClick={handleClick} />
-          <CountState2 count={count} onClick={handleClick} />
-        </div>
-      </div>
-  );
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
 }
