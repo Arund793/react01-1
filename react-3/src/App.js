222b174e1ebe35eaad0a31b9884132cb9415a3cc
Arund793 <genesis793@naver.com>
2025-05-15 16:50:47 +0900

    0515

--- react-3/src/App.js
+++ react-3/src/App.js
@@ -1,41 +1,87 @@
+import React, { useState } from 'react';
+
+
 export default function App() {
-  return <FilterableProductTable products={PRODUCTS} />;
+  return (
+    <>
+      <FilterableProductTable products={PRODUCTS} />;
+    </>
+  )
 }
 
 function FilterableProductTable({ products }) {
+  const [filterText, setFilterText] = useState('');
+  const [inStockOnly, setInStockOnly] = useState(false);
   return (
     <div>
-      <SearchBar />
-      <ProductTable products={products} />
+      <SearchBar
+      filterText={filterText}
+      inStockOnly={inStockOnly}
+      onFilterTextChange={setFilterText}
+      onInStockOnlyChange={setInStockOnly} />
+
+      <ProductTable
+        products={products}
+        filterText={filterText}
+        inStockOnly={inStockOnly} />
     </div>
   );
 }
 
 
-function SearchBar() {
+function SearchBar({
+  filterText,
+  inStockOnly,
+  onFilterTextChange,
+  onInStockOnlyChange
+}) {
   return (
-    <form>
-      <input type="text" placeholder="Search..." />
-      <label>
-        <input type="checkbox" />
-        {' '}
-        Only show products in stock
-      </label>
-    </form>
+    <form className="search-bar">
+  <input
+    type="text"
+    value={filterText}
+    placeholder="Search..."
+    onChange={(e) => onFilterTextChange(e.target.value)}
+  />
+  <label>
+    <input
+      type="checkbox"
+      checked={inStockOnly}
+      onChange={(e) => onInStockOnlyChange(e.target.checked)}
+    />
+    {' '}
+    Only show products in stock
+  </label>
+</form>
+
   );
 }
 
 
-function ProductTable({ products }) {
+
+function ProductTable({ products, filterText, inStockOnly }) {
   const rows = [];
   let lastCategory = null;
 
   products.forEach((product) => {
+    if (
+      product.name.toLowerCase().indexOf(
+        filterText.toLowerCase()
+      ) === -1
+    ) {
+      return;
+    }
+  
+    if (inStockOnly && !product.stocked) {
+      return;
+    }
+  
     if (product.category !== lastCategory) {
       rows.push(
         <ProductCategoryRow
           category={product.category}
-          key={product.category} />
+          key={product.category}
+        />
       );
     }
     rows.push(
